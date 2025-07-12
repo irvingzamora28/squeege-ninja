0b1a35a1312090aeed7e9fe26718c92c03ca49b2
Irving Zamora <imzodev@gmail.com>
2025-07-12 07:13:42 -0600

    refactor: rename CONTACT_SUBMISSIONS_TABLE constant to ALLSET_CONTACT_SUBMISSIONS_TABLE

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,7 +6,7 @@ export const SQLITE_DB_PATH = `${SQLITE_DB_DIR}/${SQLITE_DB_NAME}`
 export const SQLITE_SCHEMA_FILE = `${SQLITE_DB_DIR}/schema.sql`
 
 export const ALLSET_EMAILS_TABLE = 'allset_emails'
-export const CONTACT_SUBMISSIONS_TABLE = 'allset_contact_submissions'
+export const ALLSET_CONTACT_SUBMISSIONS_TABLE = 'allset_contact_submissions'
 export const ALLSET_EMAIL_TEMPLATE_DATA_TABLE = 'allset_email_template_data'
 export const ALLSET_CTA_CONFIG_TABLE = 'allset_cta_config'
 // The landing content prompt is now dynamic and uses templates generated from types
